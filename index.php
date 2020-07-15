bb3badcccdd470d69b22820855d0bd276848ebd9
natkaninp <68221171+natkaninp@users.noreply.github.com>
2020-07-15 15:15:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -155,7 +155,7 @@ if($command <>"NA"){
 		    
 		$arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
 		$arrayPostData['messages'][0]['type'] = "text";
-		$arrayPostData['messages'][0]['text'] = $resultsLineProfile."MP5 บันทึกข้อความเรียบร้อยค่ะ";
+		$arrayPostData['messages'][0]['text'] = "MP5 บันทึกข้อความเรียบร้อยค่ะ";
 		replyMsg($arrayHeader,$arrayPostData);
 		break;
 	default:
